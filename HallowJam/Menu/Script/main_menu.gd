c4959833fc528348b7e1a09e8a3dbbb1f9699e07
luisesales <duduosales@hotmail.com>
2023-10-14 23:59:16 -0300

    maerge main into my branch adding main menu with sprite

--- HallowJam/Menu/Script/main_menu.gd
+++ HallowJam/Menu/Script/main_menu.gd
@@ -2,22 +2,43 @@ extends MarginContainer
 
 const startScene = "res://Cenas/Map.tscn"
 
+
+
+@onready var textOne = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer/HBoxContainer/Option
+@onready var textTwo = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer2/HBoxContainer/Option
+@onready var textThree = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer3/HBoxContainer/Option
+
+const originalFontColor = Color(0.757, 0.737, 0.329)
+const originalShadowColor = Color(0.627, 0.506, 0.169)
+const selectedFontColor = Color(0.29, 0.435, 0.459)
+const selectedShadowColor = Color(0.2, 0.271, 0.373)
+
 @onready var selectorOne = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer/HBoxContainer/Selector
 @onready var selectorTwo = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer2/HBoxContainer/Selector
 @onready var selectorThree = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer3/HBoxContainer/Selector
 
 var currentSelection = 0
 
-func setCurrentSelection(currentSelection) :
+func changeColor(component,fontColor, shadowColor) :
+	component.add_theme_color_override("font_color",fontColor)
+	component.add_theme_color_override("font_shadow_color",shadowColor)
+
+func setCurrentSelection(currentSelection) :	
 	selectorOne.text = ""
 	selectorTwo.text = ""
 	selectorThree.text = ""
+	changeColor(textOne,originalFontColor,originalShadowColor)
+	changeColor(textTwo,originalFontColor,originalShadowColor)
+	changeColor(textThree,originalFontColor,originalShadowColor)
 	if currentSelection == 0 :
 		selectorOne.text = ">"
+		changeColor(textOne,selectedFontColor,selectedShadowColor)
 	elif currentSelection == 1 :
-		selectorTwo.text = ">"
+		selectorTwo.text = ">"		
+		changeColor(textTwo,selectedFontColor,selectedShadowColor)
 	else :
-		selectorThree.text = ">"	
+		selectorThree.text = ">"			
+		changeColor(textThree,selectedFontColor,selectedShadowColor)
 
 func handleSelection(currentSelection) :
 	if(currentSelection == 0) :
@@ -27,6 +48,9 @@ func handleSelection(currentSelection) :
 		
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	changeColor(selectorOne,selectedFontColor,selectedShadowColor)
+	changeColor(selectorTwo,selectedFontColor,selectedShadowColor)
+	changeColor(selectorThree,selectedFontColor,selectedShadowColor)
 	setCurrentSelection(0)
 
 
